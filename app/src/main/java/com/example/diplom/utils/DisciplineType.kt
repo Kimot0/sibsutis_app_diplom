022828fc360f76c1132379901e99c4a19c0ec4c0
egork4 <egorbauer@yandex.ru>
2023-04-16 14:41:04 +0700

    Added DisciplineType

---
+++ app/src/main/java/com/example/diplom/utils/DisciplineType.kt
@@ -0,0 +1,7 @@
+package com.example.diplom.utils
+
+enum class DisciplineType(name: String) {
+    LECTURE("Лекция"),
+    PRACTICAL("Приактическое занятие"),
+    LABORATORY("Лабораторное занятие"),
+}
