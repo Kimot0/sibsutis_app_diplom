8d885a4bc0ec84b3a0849118a7d9291eabccea3c
egork4 <egorbauer@yandex.ru>
2023-04-16 14:11:34 +0700

    Edit AttendanceFragment

--- app/src/main/java/com/example/diplom/ui/attendance/AttendanceFragment.kt
+++ app/src/main/java/com/example/diplom/ui/attendance/AttendanceFragment.kt
@@ -1,7 +1,54 @@
 package com.example.diplom.ui.attendance
 
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.diplom.R
+import com.example.diplom.databinding.AttendanceFragmentBinding
+import com.example.diplom.utils.DisciplineType
 
-class AttendanceFragment: Fragment(R.layout.attendance_fragment) {
+class AttendanceFragment : Fragment(R.layout.attendance_fragment) {
+
+    private var disciplines: MutableList<String> = mutableListOf(
+        "Технологии разработки программного обеспечения",
+        "СиАОД",
+        "Представление графической информации",
+    )
+
+    private var disciplineTypes: MutableList<String> = mutableListOf(
+        "Лекция",
+        "Практическое занятие",
+        "Лабораторное занятие"
+    )
+
+    private var students: MutableList<String> = mutableListOf(
+        "Бауэр Егор",
+        "Бондаренко Антон",
+        "Мониев Никита",
+        "Иванов Артем",
+        "Макс Ярыгин",
+        "Володимир Сотников",
+        "Fucking slaves",
+        "Boss of the gym",
+        "Boy next door"
+    )
+
+    private lateinit var binding: AttendanceFragmentBinding
+    private lateinit var disciplineNameAdapter: ArrayAdapter<String>
+    private lateinit var disciplineTypeAdapter: ArrayAdapter<String>
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = AttendanceFragmentBinding.bind(view)
+        initAdapters()
+    }
+
+    private fun initAdapters() {
+        disciplineTypeAdapter = ArrayAdapter(requireContext(), R.layout.common_item, disciplineTypes)
+        disciplineNameAdapter = ArrayAdapter(requireContext(), R.layout.common_item, disciplines)
+
+        binding.disciplineNameSpinner.adapter = disciplineNameAdapter
+        binding.disciplineTypeSpinner.adapter = disciplineTypeAdapter
+    }
 }
