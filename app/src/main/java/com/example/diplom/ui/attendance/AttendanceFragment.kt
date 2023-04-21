cb446a274d71984e98756fbb1341f2b9dfde2957
egork4 <egorbauer@yandex.ru>
2023-04-21 17:42:44 +0700

    Added Adapters

--- app/src/main/java/com/example/diplom/ui/attendance/AttendanceFragment.kt
+++ app/src/main/java/com/example/diplom/ui/attendance/AttendanceFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.diplom.R
 import com.example.diplom.databinding.AttendanceFragmentBinding
 import com.example.diplom.utils.DisciplineType
@@ -23,20 +24,28 @@ class AttendanceFragment : Fragment(R.layout.attendance_fragment) {
     )
 
     private var students: MutableList<String> = mutableListOf(
+        "Александрова Ангелина",
         "Бауэр Егор",
         "Бондаренко Антон",
+        "Давыдков Никита",
+        "Зырянов Константин",
+        "Иванов Артём",
+        "Исмаилов Рухид",
+        "Истомина Анна",
+        "Косарева Екатерина",
+        "Костин Кирилл",
+        "Кульбезекова Кристина",
+        "Меньщиков Данил",
         "Мониев Никита",
-        "Иванов Артем",
-        "Макс Ярыгин",
-        "Володимир Сотников",
-        "Fucking slaves",
-        "Boss of the gym",
-        "Boy next door"
+        "Подковыров Артем",
+        "Сотников Владимир"
     )
 
     private lateinit var binding: AttendanceFragmentBinding
     private lateinit var disciplineNameAdapter: ArrayAdapter<String>
     private lateinit var disciplineTypeAdapter: ArrayAdapter<String>
+
+    private val adapter: AttendanceRecyclerAdapter = AttendanceRecyclerAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -50,5 +59,10 @@ class AttendanceFragment : Fragment(R.layout.attendance_fragment) {
 
         binding.disciplineNameSpinner.adapter = disciplineNameAdapter
         binding.disciplineTypeSpinner.adapter = disciplineTypeAdapter
+
+        binding.studentsRecyclerView.adapter = adapter
+        binding.studentsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        adapter.setUpdatedData(students)
     }
 }
