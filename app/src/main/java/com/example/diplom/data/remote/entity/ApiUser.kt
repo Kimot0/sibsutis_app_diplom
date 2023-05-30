4596df3ac22b50b07386f4f40a89d5904a94b5c4
egork4 <egorbauer@yandex.ru>
2023-05-30 20:03:13 +0700

    Changed JsonClass

--- app/src/main/java/com/example/diplom/data/remote/entity/ApiUser.kt
+++ app/src/main/java/com/example/diplom/data/remote/entity/ApiUser.kt
@@ -1,5 +1,6 @@
 package com.example.diplom.data.remote.entity
 
+import com.example.diplom.domain.entity.Role
 import com.example.diplom.domain.entity.UserAuthResult
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
@@ -7,8 +8,15 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class ApiUser(
     @Json(name = "group")
-    val group: String
+    val group: String,
+    @Json(name = "role")
+    val role: Role,
+    @Json(name = "fio")
+    val fio: String
 )
 fun ApiUser?.toUserAuthResult() = UserAuthResult(
-    group = this?.group ?: ""
+    group = this?.group ?: "",
+    role = this?.role ?: Role.STUDENT,
+    fio = this?.fio ?: "Unknown user",
 )
+
