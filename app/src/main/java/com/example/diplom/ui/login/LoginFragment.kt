5eccd71cb03628e3d5663ff1864dd1756d11237c
egork4 <egorbauer@yandex.ru>
2023-05-30 20:15:40 +0700

    Created fun onLoginSuccess

--- app/src/main/java/com/example/diplom/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/diplom/ui/login/LoginFragment.kt
@@ -2,17 +2,15 @@ package com.example.diplom.ui.login
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.diplom.NavigationActivity
 import com.example.diplom.R
-import com.example.diplom.data.dataSource.database.InMemoryCache
 import com.example.diplom.databinding.LoginFragmentBinding
-import com.example.diplom.domain.entity.Account
-import com.example.diplom.domain.entity.ScheduleRequest
-import com.example.diplom.domain.entity.UserAuthRequest
+import com.example.diplom.domain.Requests
+import com.example.diplom.domain.entity.Role
+import com.example.diplom.domain.entity.UserAuthResult
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -23,54 +21,64 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class LoginFragment : Fragment(R.layout.login_fragment) {
 
     private lateinit var binding: LoginFragmentBinding
-    private val model: LoginViewModel by viewModel()
+    private val viewModel: LoginViewModel by viewModel()
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = LoginFragmentBinding.bind(view)
         bindUi()
+        collectData()
+    }
+
+    private fun collectData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.authState.collect {
+                when (it) {
+                    is Requests.Success -> {
+                        onLoginSuccess(it.data)
+                    }
+
+                    else -> {
+
+                    }
+                }
+            }
+        }
     }
 
     private fun bindUi() {
         with(binding) {
-            LoginButton.setOnClickListener{
+            LoginButton.setOnClickListener {
                 it.isClickable = false
-                lifecycleScope.launch {
-                    login()
-                    it.isClickable = true
-                }
+                login()
+                it.isClickable = true
             }
         }
     }
 
-    private suspend fun login() {
+    private fun login() {
         with(binding) {
-            val role = "TEACHER"
-            val login = loginEditText.text.toString()
-            val pass = passwordEditText.text.toString()
-            if (logs.contains(login) && passwords.contains(pass)) {
-                Toast.makeText(
-                    context,
-                    "Your login: $login \nyour password: $pass",
-                    Toast.LENGTH_SHORT
-                ).show()
-                // TODO: Получать данные с сервера
-                when (role) {
-                    "TEACHER" -> {
-                        (requireActivity() as NavigationActivity).setupBottomNavigationBarForTeacher()
-                    }
-                    "STUDENT" -> {
-                        (requireActivity() as NavigationActivity).setupBottomNavigationBarForStudent()
-                    }
-                    "HEAD" -> {
-                        (requireActivity() as NavigationActivity).setupBottomNavigationBarForHead()
-                    }
-                }
-                findNavController().navigate(R.id.action_navigation_login_to_navigation_news)
-            } else {
-                Toast.makeText(context, "ERROR", Toast.LENGTH_SHORT).show()
-            }
+            val login = loginEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
+            viewModel.auth(login, password)
         }
+    }
+
+    private fun onLoginSuccess(user: UserAuthResult) {
+        viewModel.saveUser(user)
+        when (user.role) {
+            Role.STUDENT -> {
+                (requireActivity() as NavigationActivity).setupBottomNavigationBarForStudent()
+            }
+
+            Role.TEACHER -> {
+                //TODO Подумать что делать для препода
+                (requireActivity() as NavigationActivity).setupBottomNavigationBarForTeacher()
+            }
+
+            Role.HEAD -> { viewModel.getGroup(user.group) }
+        }
+        findNavController().navigate(R.id.action_navigation_login_to_navigation_news)
     }
 }
